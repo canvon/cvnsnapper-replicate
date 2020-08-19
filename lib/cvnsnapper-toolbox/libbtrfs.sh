8d5f5c00a5cdf6a6956752d9adba45110ad97cf8
Fabian Pietsch <fabian@canvon.de>
2020-08-19 14:31:13 +0200

    replicate-{send,receive}: Factor out subvol_get{,_received}_uuid() to libbtrfs.sh

--- lib/cvnsnapper-toolbox/libbtrfs.sh
+++ lib/cvnsnapper-toolbox/libbtrfs.sh
@@ -15,3 +15,54 @@ is_subvol() {
 
 	return 0
 }
+
+subvol_get_uuid() {
+	if ! [ "$#" -eq 1 ]
+	then
+		warn "Internal problem: subvol_get_uuid called with argument count $# instead of 1; returning false"
+		return 1
+	fi
+
+	local CST_SUBVOL="$1"; shift
+
+	local CST_UUID=
+	CST_UUID="$(btrfs subvolume show "$CST_SUBVOL" | sed -n -e 's/^\s*UUID:\s*\([-0-9a-f]*\)$/\1/ip')" || return 1
+	[ -n "$CST_UUID" ] || return 1
+	[ "$CST_UUID" = "-" ] && return 1
+
+	echo "$CST_UUID"
+	return 0
+}
+
+subvol_get_received_uuid() {
+	if ! [ "$#" -eq 1 ]
+	then
+		warn "Internal problem: subvol_get_received_uuid called with argument count $# instead of 1; returning false"
+		return 1
+	fi
+
+	local CST_SUBVOL="$1"; shift
+
+	local CST_RECEIVED_UUID=
+	CST_RECEIVED_UUID="$(btrfs subvolume show "$CST_SUBVOL" | sed -n -e 's/^\s*Received UUID:\s*\([-0-9a-f]*\)$/\1/ip')"
+	if [ -z "$CST_RECEIVED_UUID" ]
+	then
+		# Work-around for older btrfs tool versions,
+		# which don't give out received UUID in "show".
+		# So, use "list", instead. As we don't know
+		# the path that is part of the btrfs,
+		# match via subvolume UUID.
+
+		local CST_UUID=
+		CST_UUID="$(subvol_get_uuid "$CST_SUBVOL")" || return 1
+
+		# -u: UUID, -R: received UUID
+		CST_RECEIVED_UUID="$(btrfs subvolume list -u -R "$CST_SUBVOL" | sed -n -e "s/.* received_uuid \\([-0-9a-f]*\\) uuid $CST_UUID .*/\\1/ip")" || return 1
+	fi
+
+	[ -n "$CST_RECEIVED_UUID" ] || return 1
+	#[ "$CST_RECEIVED_UUID" = "-" ] && return 1
+
+	echo "$CST_RECEIVED_UUID"
+	return 0
+}
